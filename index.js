7f87a2c27193b295084472b330995786ebea1b21
Julien Paltrinieri <julpal99@gmail.com>
2024-09-27 18:30:49 -0500

    Fixed buttons on Netlify

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let homeScore = document.getElementById("home-score-el")
+/*let homeScore = document.getElementById("home-score-el")
 let guestScore = document.getElementById("guest-score-el")
 let home = 0
 let guest = 0
@@ -31,4 +31,41 @@ function addGuest2() {
 function addGuest3() {
     guest += 3
     guestScore.textContent = guest
-}
+}*/
+
+let homeScore = document.getElementById("home-score-el");
+let guestScore = document.getElementById("guest-score-el");
+let home = 0;
+let guest = 0;
+
+// HOME Buttons
+document.getElementById('home-add1').addEventListener('click', () => {
+  home += 1;
+  homeScore.textContent = home;
+});
+
+document.getElementById('home-add2').addEventListener('click', () => {
+  home += 2;
+  homeScore.textContent = home;
+});
+
+document.getElementById('home-add3').addEventListener('click', () => {
+  home += 3;
+  homeScore.textContent = home;
+});
+
+// GUEST Buttons
+document.getElementById('guest-add1').addEventListener('click', () => {
+  guest += 1;
+  guestScore.textContent = guest;
+});
+
+document.getElementById('guest-add2').addEventListener('click', () => {
+  guest += 2;
+  guestScore.textContent = guest;
+});
+
+document.getElementById('guest-add3').addEventListener('click', () => {
+  guest += 3;
+  guestScore.textContent = guest;
+});
